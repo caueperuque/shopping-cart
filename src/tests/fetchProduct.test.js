3fb7bcc4aa142da75f3ad9b70dd2d10fb3800259
caue peruque <caue19052003@gmail.com>
2023-03-22 14:43:38 -0300

    iniciando projeto

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -2,7 +2,7 @@ import './mocks/fetchSimulator';
 import { fetchProduct } from '../helpers/fetchFunctions';
 import product from './mocks/product';
 
-// implemente seus testes aqui
+// implemente seus testes  aqui
 describe('Teste a função fetchProduct', () => {
   it('...', () => {
     
