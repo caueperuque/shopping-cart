25823ac68120412b5c045ed320ac940d4e0aedf0
caue peruque <caue19052003@gmail.com>
2023-03-22 17:49:19 -0300

    requisito 3 finalizado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-console.log(fetchProductsList('computador'));
+const productList = await fetchProductsList('computador');
+const products = document.querySelector('.products');
+productList.forEach((product) => {
+  const createProduct = createProductElement(product);
+  products.appendChild(createProduct);
+});
