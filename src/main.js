eabf274a794460d65c57d8eabdd1eec87bd086ab
caue peruque <caue19052003@gmail.com>
2023-03-24 15:31:38 -0300

    resolvendo erros ao deploy

--- src/main.js
+++ src/main.js
@@ -16,61 +16,65 @@ import {
   getSavedCartIDs,
 } from './helpers/cartFunctions';
 
-window.onload = () => {
-  const savedValue = localStorage.getItem('subtotal');
-  if (savedValue) {
-    const teste = document.querySelector('.total-price');
-    teste.innerHTML = savedValue;
+async function main() {
+  window.onload = () => {
+    const savedValue = localStorage.getItem('subtotal');
+    if (savedValue) {
+      const teste = document.querySelector('.total-price');
+      teste.innerHTML = savedValue;
+    }
+  };
+
+  document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+  try {
+    const products = document.querySelector('.products');
+
+    const paragraph = document.createElement('h2');
+    paragraph.innerHTML = '...carregando';
+    paragraph.classList.add('loading');
+    products.appendChild(paragraph);
+
+    const productList = await fetchProductsList('computador');
+    // console.log(productList);
+    productList.forEach((product) => {
+      const createProduct = createProductElement(product);
+      products.appendChild(createProduct);
+    });
+
+    paragraph.remove();
+  } catch (err) {
+    const errorMsg = document.createElement('h2');
+    errorMsg.classList.add('error');
+    errorMsg.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    document.querySelector('.loading').remove();
+    document.body.append(errorMsg);
   }
-};
 
-document.querySelector('.cep-button').addEventListener('click', searchCep);
+  const productsGetStorage = async () => {
+    const idList = getSavedCartIDs();
+    const promisseList = idList.map((id) => fetchProduct(id));
+    const getProduct = await Promise.all(promisseList);
+    const getCart = document.querySelector('.cart__products');
+    getProduct.forEach((id) => getCart
+      .appendChild(createCartProductElement(id)));
+  };
 
-try {
-  const products = document.querySelector('.products');
+  await productsGetStorage();
 
-  const paragraph = document.createElement('h2');
-  paragraph.innerHTML = '...carregando';
-  paragraph.classList.add('loading');
-  products.appendChild(paragraph);
-
-  const productList = await fetchProductsList('computador');
-  // console.log(productList);
-  productList.forEach((product) => {
-    const createProduct = createProductElement(product);
-    products.appendChild(createProduct);
-  });
-
-  paragraph.remove();
-} catch (err) {
-  const errorMsg = document.createElement('h2');
-  errorMsg.classList.add('error');
-  errorMsg.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
-  document.querySelector('.loading').remove();
-  document.body.append(errorMsg);
+  const addProductInCart = () => {
+    const addCartBtn = document.querySelectorAll('.product__add');
+    const getProduct = document.querySelectorAll('.product__id');
+    const getCart = document.querySelector('.cart__products');
+    addCartBtn.forEach((btn, index) => btn.addEventListener('click', async () => {
+      const product = getProduct[index].innerHTML;
+      saveCartID(product);
+      const addInCart = createCartProductElement(await fetchProduct(product));
+      getCart.appendChild(addInCart);
+      calculatePrice();
+    }));
+  };
+  addProductInCart();
 }
 
-const productsGetStorage = async () => {
-  const idList = getSavedCartIDs();
-  const promisseList = idList.map((id) => fetchProduct(id));
-  const getProduct = await Promise.all(promisseList);
-  const getCart = document.querySelector('.cart__products');
-  getProduct.forEach((id) => getCart
-    .appendChild(createCartProductElement(id)));
-};
-
-await productsGetStorage();
-
-const addProductInCart = () => {
-  const addCartBtn = document.querySelectorAll('.product__add');
-  const getProduct = document.querySelectorAll('.product__id');
-  const getCart = document.querySelector('.cart__products');
-  addCartBtn.forEach((btn, index) => btn.addEventListener('click', async () => {
-    const product = getProduct[index].innerHTML;
-    saveCartID(product);
-    const addInCart = createCartProductElement(await fetchProduct(product));
-    getCart.appendChild(addInCart);
-    calculatePrice();
-  }));
-};
-addProductInCart();
+main();
