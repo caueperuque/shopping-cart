f5cabca33c774f5ae8c8515b37f93c7932246ecd
caue peruque <caue19052003@gmail.com>
2023-03-24 14:32:13 -0300

    requisito 11 finalizado

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,22 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (CEP) => {
+  const AWESOME_API = fetch(`https://cep.awesomeapi.com.br/json/${CEP}`);
+  const BRASIL_API = fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}`);
+  const promises = [AWESOME_API, BRASIL_API];
+  const firstResponse = await Promise.any(promises);
+  const data = await firstResponse.json();
+  return data;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const CEP = document.querySelector('.cep-input').value;
+  const printAddress = document.querySelector('.cart__address');
+  try {
+    const { address, district, city, state } = await getAddress(CEP);
+    printAddress.innerHTML = `${address} - ${district} - ${city} - ${state}`;
+    if (printAddress.innerHTML.includes('undefined')) {
+      throw new Error('CEP não encontrado');
+    }
+  } catch (err) {
+    printAddress.innerHTML = 'CEP não encontrado';
+  }
 };
